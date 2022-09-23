620e0b13452d18eb4fe403409308ca7af4f9c86f
nurikiitu <zhedelbaevn@mail.ru>
2022-09-23 23:33:19 +0600

    CafeApp

---
+++ app/src/main/java/com/example/cafe/OrderDetailActivity.java
@@ -0,0 +1,65 @@
+ package com.example.cafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+ public class OrderDetailActivity extends AppCompatActivity {
+    private TextView textViewName;
+     private TextView textViewDrink;
+     private TextView textViewDrinkType;
+     private TextView textViewAdditives;
+
+
+
+
+     private static final String EXTRA_USER_NAME = "userName";
+    private static final String EXTRA_DRINK = "drink";
+    private static final String EXTRA_DRINK_TYPE = "drinkType";
+    private static final String EXTRA_ADDITIVES = "additives";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_detail);
+        initViews();
+        Intent intent = getIntent();
+        textViewName.setText(intent.getStringExtra(EXTRA_USER_NAME));
+        textViewDrink.setText(intent.getStringExtra(EXTRA_DRINK));
+        textViewDrinkType.setText(intent.getStringExtra(EXTRA_DRINK_TYPE));
+        textViewAdditives.setText(intent.getStringExtra(EXTRA_ADDITIVES));
+
+
+
+    }
+
+    private void initViews(){
+          textViewName = findViewById(R.id.textViewName) ;
+          textViewDrink = findViewById(R.id.textViewDrink);
+          textViewDrinkType = findViewById(R.id.textViewDrinkType);
+          textViewAdditives = findViewById(R.id.textViewAdditives);
+    }
+
+
+
+
+    public static Intent newIntent(
+            Context context,
+            String userName,
+            String drink,
+            String drinkType,
+            String additives
+
+            ){
+        Intent intent = new Intent(context,OrderDetailActivity.class);
+        intent.putExtra(EXTRA_USER_NAME,userName);
+        intent.putExtra(EXTRA_DRINK,drink);
+        intent.putExtra(EXTRA_DRINK_TYPE,drinkType);
+        intent.putExtra(EXTRA_ADDITIVES,additives);
+        return intent;
+
+    }
+}
